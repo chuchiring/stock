c9a7042e643c118ff63993b57c0e44e883576c33
chuchiring <chu.chiring@gmail.com>
2019-02-17 14:14:22 +0800

    commit first version

--- leetcode/topic149/main.go
+++ leetcode/topic149/main.go
@@ -6,11 +6,12 @@ import (
 )
 
 type (
-	point struct {
+	//Point include x, y
+	Point struct {
 		x, y int
 	}
 	pointsSorter struct {
-		data []point
+		data []Point
 	}
 )
 
@@ -23,9 +24,10 @@ func (p *pointsSorter) Less(i, j int) bool {
 	return p.data[i].x < p.data[j].x
 }
 
-func maxPoints(points []point) int {
-	// arr := &pointsSorter(points)
-	// sort.Sort(arr)
+func maxPoints(points []Point) int {
+	debuglog := true
+	arr := &pointsSorter{points}
+	sort.Sort(arr)
 
 	xarr, yarr := make([]int, len(points)), make([]int, len(points))
 	for i := range points {
@@ -50,18 +52,72 @@ func maxPoints(points []point) int {
 
 	rowMax, colMax := fCalcMaxLenOfXY(xarr), fCalcMaxLenOfXY(yarr)
 
-	fmt.Println("y:", yarr)
-	fmt.Println("x:", xarr)
-	fmt.Printf("列直线: %d, 行直线: %d\n", rowMax, colMax)
-
-	if colMax > rowMax {
-		return colMax
+	//计算斜线
+	fXX := func(arr []Point, mode int) int {
+		maxcount := 2
+		for i := range arr {
+			pstart := arr[i]
+			curLen := 1
+			for j := i + 1; j < len(arr); j++ {
+				//判断是否在直线
+				if arr[j].x-pstart.x == arr[j].y-pstart.y {
+					curLen++
+				}
+			}
+			if curLen > maxcount {
+				maxcount = curLen
+			}
+		}
+		return maxcount
 	}
-	return colMax
+
+	zxMax := fXX(points, 1)
+
+	fFX := func(arr []Point, mode int) int {
+		maxcount := 2
+		for i := len(arr) - 1; i >= 0; i-- {
+			pstart := arr[i]
+			curLen := 1
+			for j := i - 1; j >= 0; j-- {
+				//判断是否在直线
+				if arr[j].x-pstart.x == pstart.y-arr[j].y {
+					curLen++
+				}
+			}
+			if curLen > maxcount {
+				maxcount = curLen
+			}
+		}
+		return maxcount
+	}
+
+	fxMax := fFX(points, 1)
+
+	if debuglog {
+		fmt.Println("a:", *arr)
+		fmt.Println("y:", yarr)
+		fmt.Println("x:", xarr)
+		fmt.Printf("列直线: %d, 行直线: %d, 正斜: %d, 反斜: %d\n", rowMax, colMax, zxMax, fxMax)
+	}
+
+	max := colMax
+	if rowMax > max {
+		max = rowMax
+	}
+
+	if zxMax > max {
+		max = zxMax
+	}
+
+	if fxMax > max {
+		max = fxMax
+	}
+	return max
 }
 
 func main() {
-	points := []point{{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}}
+	// points := []point{{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}}
+	points := []Point{{1, 1}, {2, 2}, {3, 3}}
 	fmt.Println("p:", points)
 	fmt.Println("直线最大: ", maxPoints(points))
 }
--- leetcode/topic149/main_test.go
+++ leetcode/topic149/main_test.go
@@ -4,14 +4,14 @@ import "testing"
 
 func Test_maxPoints(t *testing.T) {
 	type args struct {
-		points []point
+		points []Point
 	}
 	tests := []struct {
 		name string
 		args args
 		want int
 	}{
-		{"simple", args{[]point{{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}}}, 4},
+		{"simple", args{[]Point{{1, 1}, {3, 2}, {5, 3}, {4, 1}, {2, 3}, {1, 4}}}, 4},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
