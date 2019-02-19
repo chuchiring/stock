bfaba7660212114037f48b2feb5d364d8c4816d0
chuchiring <chu.chiring@gmail.com>
2019-02-19 22:54:52 +0800

    need try agin...damn

--- leetcode/src/topic862/main.go
+++ leetcode/src/topic862/main.go
@@ -4,38 +4,59 @@ import "fmt"
 
 func shortestSubarray(A []int, K int) int {
 	if len(A) == 0 {
-		return 0
+		return -1
 	}
 
-	minLen := -1
+	//开始正式计算，最后一次全加法和第一次不用计算了
+	low, high, arrLen := 0, len(A)-1, len(A)
+	lastFoundIndex := arrLen + 1
 
-	resultArr := make([]int, len(A))
+	for low <= high {
+		mid := (low + high) >> 1
 
-	for i := 0; i < len(A); i++ {
-		resultArr[i] = A[i]
-		if resultArr[i] >= K {
-			return 1
-		}
-	}
+		num := 0
+		found := false
 
-	deepth := 1
-	for deepth < len(A) {
-		for i := 0; i < len(A); i++ {
-			if i+deepth < len(A) {
-				resultArr[i] = resultArr[i] + A[i+deepth]
-				if resultArr[i] >= K {
-					return deepth + 1
+		//计算连续mid个连续的值
+		for i := 0; i < arrLen; i++ {
+			if i+mid < arrLen {
+				if i == 0 {
+					for j := 0; j <= mid; j++ {
+						num = num + A[j]
+					}
+				} else {
+					num = num - A[i-1] + A[i+mid]
+				}
+				//判断总和是不是大于K
+				if num >= K {
+					found = true
+					break
 				}
 			}
 		}
-		deepth++
+
+		//继续查找
+		if found {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+
+		if found && mid < lastFoundIndex {
+			lastFoundIndex = mid
+		}
+
 	}
 
-	return minLen
+	if lastFoundIndex == arrLen+1 {
+		return -1
+	}
+
+	return lastFoundIndex + 1
 }
 
 func main() {
-	v := shortestSubarray([]int{2, -1, 2}, 3)
+	v := shortestSubarray([]int{44, -25, 75, -50, -38, -42, -32, -6, -40, -47}, 19)
 
 	fmt.Println(v)
 }
---
+++ leetcode/src/topic862/main_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func Test_shortestSubarray(t *testing.T) {
+	type args struct {
+		A []int
+		K int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"s1", args{[]int{1}, 1}, 1},
+		{"s2", args{[]int{1, 2}, 4}, -1},
+		{"s3", args{[]int{2, -1, 2}, 3}, 3},
+		{"s4", args{[]int{17, 85, 93, -45, -21}, 150}, 2},
+		{"s5", args{[]int{56, -21, 56, 35, -9}, 61}, 2},
+		{"s6", args{[]int{44, -25, 75, -50, -38, -42, -32, -6, -40, -47}, 19}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := shortestSubarray(tt.args.A, tt.args.K); got != tt.want {
+				t.Errorf("shortestSubarray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
