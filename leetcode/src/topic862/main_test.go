935ed7a53d853779ff2b65d237548f07e361a5f9
chuchiring <chu.chiring@gmail.com>
2019-02-21 21:49:44 +0800

    add test case

--- leetcode/src/topic862/main_test.go
+++ leetcode/src/topic862/main_test.go
@@ -20,6 +20,7 @@ func Test_shortestSubarray(t *testing.T) {
 		{"s5", args{[]int{56, -21, 56, 35, -9}, 61}, 2},
 		{"s6", args{[]int{44, -25, 75, -50, -38, -42, -32, -6, -40, -47}, 19}, 1},
 		{"s7", args{[]int{27, 20, 79, 87, -36, 78, 76, 72, 50, -26}, 453}, 9},
+		{"s8", args{[]int{1, -1, -1, -1, -1}, 1}, 1},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
